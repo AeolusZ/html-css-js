cfed8ed89056b41f29e89c695a4c5176dea9a965
ruxiu.zheng <18202764523@163.com>
2019-06-05 19:37:35 +0800

    添加xss防御攻击

---
+++ js/func.js
@@ -0,0 +1,15 @@
+// window.onload = function() {
+//     try{
+//         const newDiv1 = document.createElement('div')
+//         newDiv1.innerHTML = '小张同学'
+//         console.log(document.getElementById('root'))
+//         document.getElementById('root').appendChild(newDiv1)
+//     } catch{
+//         console.log(document.getElementById('root'))
+//     }
+// }
+   
+console.log('2', document.getElementById('root'))
+
+
+// console.log(document.getElementById('root'))
